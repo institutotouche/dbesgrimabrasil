dac422ac3803a67259343f3e6aeffc582c29c850
vbfall <vbfall@gmail.com>
2020-07-27 20:13:23 -0400

    vf split creation and insertion for StatusCombates

---
+++ db-setup/insert_statuscombates.sql
@@ -0,0 +1,10 @@
+INSERT INTO
+	StatusCombates(Status)
+VALUES
+	("Finalizado"),
+	("Lesao atleta 1"),
+	("Lesao atleta 2"),
+	("WO atleta 1"),
+	("WO atleta 2"),
+	("Cartao preto atleta 1"),
+	("Cartao preto atleta 2")
