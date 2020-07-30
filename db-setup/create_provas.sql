ef31e87c99f7684004e9b22baceb1d81b01b00b4
vbfall <vbfall@gmail.com>
2020-07-29 21:54:46 -0400

    vf adjust datatypes

--- db-setup/create_provas.sql
+++ db-setup/create_provas.sql
@@ -2,11 +2,11 @@ CREATE TABLE IF NOT EXISTS Provas (					-- Cada registro uma prova dentro de um 
 	`Index` BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,	-- Indice primario da tabela
 	IndexEvento BIGINT NOT NULL,	-- Referencia ao evento
 	Inicio_datetime DATETIME,		-- Horario de inicio
-	Arma VARCHAR(255),		-- Espada / Florete / Sabre
-	CategoriaIdade VARCHAR(255),	-- Livre / Cadete / Veterano, etc
-	CategoriaCadeiraDeRodas VARCHAR(255),	-- A / B / C (NULL se não cadeirante)
+	Arma VARCHAR(15),		-- Espada / Florete / Sabre
+	CategoriaIdade VARCHAR(50),	-- Livre / Cadete / Veterano, etc
+	CategoriaCadeiraDeRodas CHAR,	-- A / B / C (NULL se não cadeirante)
 	Sexo CHAR,		-- Sexo da prova, 'm' ou 'f'
-	EquipeOuIndividual VARCHAR(255),	-- Equipe / Individual
+	EquipeOuIndividual VARCHAR(15),	-- Equipe / Individual
 	IndexFonte BIGINT,									-- Indice de referencia da ultima fonte de informacao
 	last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP	-- Momento da ultima atualizacao
 )
