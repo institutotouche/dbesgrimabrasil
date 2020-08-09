76e2fc56e6785786d2278f2ea755c1e472661dca
vbfall <vbfall@gmail.com>
2020-08-08 22:08:37 -0400

    vf remove field _parcial_ from table of individual matches

--- db-setup/create_combatesindividuais.sql
+++ db-setup/create_combatesindividuais.sql
@@ -2,7 +2,6 @@ CREATE TABLE IF NOT EXISTS CombatesIndividuais (					-- Cada registro um combate
 	`Index` BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,	-- Indice primario da tabela
 	IndexProva BIGINT NOT NULL, 	-- Referencia a prova em que o combate aconteceu
 	Rodada VARCHAR(255), 					-- Rodada da prova (Final, semi, Q8, Q16, poule, etc)
-	Parcial INT,									-- Qual parcial do encontro corresponde o combate (número de 1 a 9 em encontros normais por equipes a 45 pontos)
 	IndexAtleta1 BIGINT,	-- Referencia atleta 1
 	IndexAtleta2 BIGINT,	-- Referencia atleta 2
 	PontosAtleta1 INT,		-- Pontuacao atleta 1 ao final desse combate
